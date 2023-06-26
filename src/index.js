c91112aa1970d25bb636809c58d48cbc8c05cf3f
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-06-26 20:39:22 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import {initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs, addDoc, deleteDoc, doc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgaIKzthfUBquCUSVEyDa8YYT_L7709jA",
+    authDomain: "fir-9-dojo-668f8.firebaseapp.com",
+    projectId: "fir-9-dojo-668f8",
+    storageBucket: "fir-9-dojo-668f8.appspot.com",
+    messagingSenderId: "314166565831",
+    appId: "1:314166565831:web:ed4635d6958c7649c2d7e2"
+  };
+
+//   initialize firebase app
+initializeApp(firebaseConfig)
+
+//   initialize services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot)=>{
+    // console.log(snapshot.docs)
+    let books = []
+    snapshot.docs.forEach((doc)=>{
+        books.push({...doc.data(), id: doc.id})
+    })
+    console.log(books);
+})
+.catch((err)=>console.log(err.message))
+
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// delete documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
