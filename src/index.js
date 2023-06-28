8955effd22edd55ff4c9a85b7deb5c589194974d
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-06-28 15:55:07 +0300

    logging in/out

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import {
 } from 'firebase/firestore'
 import {
     getAuth,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signOut
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -123,6 +124,35 @@ signupForm.addEventListener('submit', (e) => {
     .then(cred => {
       console.log('user created:', cred.user)
       signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
     })
     .catch(err => {
       console.log(err.message)
